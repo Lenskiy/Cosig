57a5690e29594db05f1671b2a7b04da027b6f07f
Lenskiy <a.a.lensky@gmail.com>
2016-03-15 19:59:53 +0900

    Initial Commit

---
+++ Sigcomp/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Sigcomp
+//
+//  Created by Artem Lenskiy on 15/03/2016.
+//  Copyright © 2016 Artem Lenskiy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
